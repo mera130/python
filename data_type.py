966878e5d0ef9a2e417892016470b591b296d03e
mera130 <mera2011.11.3@gmail.com>
2025-06-06 22:08:46 +0600

    data type

---
+++ data_type.py
@@ -0,0 +1,20 @@
+Name = "mera"
+Age = 60
+is_student = True
+weight = 100
+
+print("your name is", name)
+print("Data type of name is," type(name))
+print("your age is", Age)
+print("Data type of name is", type(age))
+print("is student", is_student)
+print(" Data type of is_student", type(is student ))
+
+
+# after casting 
+
+print("after type casting")
+age = str(age)
+print("Data type of age" , type(age))
+weight = int(weight)
+print("data type of weight", type(weight))
