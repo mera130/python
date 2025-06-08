73c039a2060e1abbc80d72ffebef972553a76d80
mera130 <mera2011.11.3@gmail.com>
2025-06-08 16:04:25 +0600

    operator

---
+++ operatoe.py
@@ -0,0 +1,21 @@
+num 1= 45
+num 2 = 3
+
+print("number 1", num1 )
+print("number 2", num2)
+print("addition :", num1 + num2)
+print("difference :", num1-num2)
+print("product :", num1*num2)
+print(" devision :", num1/num2)
+print("floor devision :" , num1//num2)
+print("modulus:", num1%num2)
+print("square :", num2**2)
+print("square root :", num1**0.5)
+
+print("equal ?", num1==num2)
+print("number 1 greater?" num1>num2)
+print("number 2 greater?" num1<num2)
+print("not equal?", num1!=num2)
+
+result = num1/2+num2**2+10
+print("result of given equation is :", result)
