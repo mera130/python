1af9d3be5f65583d4a65eaca5789d82bbcdcb4df
mera130 <mera2011.11.3@gmail.com>
2025-06-21 21:40:46 +0600

    absolute value

---
+++ absolute_value.py
@@ -0,0 +1,9 @@
+def absolute_value(num):
+    if num >= 0:
+        return num 
+
+    else:
+        return -num
+
+print("the absolute value of 69:", absolute_value(69))  
+print("the absolute value of -80:" , absolute_value(-80))    
