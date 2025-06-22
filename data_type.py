38de852d5037e44a9b0d8074418e17cca3bb59a7
mera130 <mera2011.11.3@gmail.com>
2025-06-22 21:36:17 +0600

    polygons

--- data_type.py
+++ data_type.py
@@ -4,11 +4,11 @@ is_student = True
 weight = 100
 
 print("your name is", name)
-print("Data type of name is," type(name))
+print("Data type of name is", type(name))
 print("your age is", Age)
 print("Data type of name is", type(age))
 print("is student", is_student)
-print(" Data type of is_student", type(is student ))
+print(" Data type of is_student", type(is_student ))
 
 
 # after casting 
---
+++ polygonss.py
@@ -0,0 +1,26 @@
+import turtle
+turtle.Screen().bgcolor("green")
+board = turtle.turtle()
+
+# triangle
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+#square
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
