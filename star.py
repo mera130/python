480346352c24420facb8836ebe33c72c3125d16b
mera130 <mera2011.11.3@gmail.com>
2025-06-24 16:34:06 +0600

    star

---
+++ star.py
@@ -0,0 +1,28 @@
+import turtle
+
+turtle.Screen().bgcolor("red")
+board = turtle.turtles()
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.penup
+board.right(150)
+board.forward(50)
+
+#second triangle of tthe star
+
+board.pendown
+board.right(90)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
+
+turtle.done()
