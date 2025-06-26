0a416b138e480a0cfb902a5fce3064ef18f41e9c
mera130 <mera2011.11.3@gmail.com>
2025-06-26 20:13:14 +0600

    prime no.

---
+++ con_string.py
@@ -0,0 +1,15 @@
+first_name = "mehrish"
+last_name = "ishra"
+full_name = first_name+last_name
+example = "i luv food"*5
+
+print("first name:", first_name)
+print("last name:", last_name)
+print("full name:", full_name)
+
+word = "coding"
+print("length of strings:" , len(word))
+print("first letter of string:" ,word[0])
+print("last letter of string:", word[5])
+print("string sliced", word[0.3])
+
