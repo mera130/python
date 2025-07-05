34ea4f2023650f739e4f72329fdf6f6d02e823a0
mera130 <mera2011.11.3@gmail.com>
2025-07-05 21:49:12 +0600

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+my_dic = {}
+
+my_dic = { 1 :"apple", 2 : "ball"}
+
+my_dic = { "name" : "maddy", 1 : [2,4,3]}
+
+my_dic = {"name" : "maddy", "age": 97}
+
+print(my_dic.get("name"))
+print(my_dic["age"])
---
+++ rectangle.py
@@ -0,0 +1,18 @@
+class rectangle():
+
+
+    def__init__(self,1,0):
+        self.length = 1
+        self.width = w
+        
+    def rectangle_area(self):
+        return self.length*self.width
+    
+    
+newrectangle = rectangle(12,10) 
+print("dimension of rectangle - length : %d : %d" %
+      (newrectangle.length, newrectanle.width))
+print("area of rectanle :", newrectangle.rectanle_area()) 
+
+
+    
