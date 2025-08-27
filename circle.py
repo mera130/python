3c607cdc61bfa1b5e4d7cc3e58e2669272c17780
mera130 <mera2011.11.3@gmail.com>
2025-08-27 23:12:42 +0600

    circle

---
+++ circle.py
@@ -0,0 +1,12 @@
+print("Circle Calculator")
+r = float(input("Enter radius: "))
+
+pi = 3.14
+perimeter = 2 * pi * r
+area = pi * r * r
+
+# perimeter
+print("Perimeter:", round(perimeter, 2))
+
+# area
+print("Area:", round(area, 2))
